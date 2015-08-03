097c2acaa32c2bf7fca61ee9c12a9e416de4217a
wtaps <tonyleung187@gmail.com>
2015-08-03 14:35:38 +0800

    change 2.1.py

--- 2/2.1.py
+++ 2/2.1.py
@@ -10,3 +10,8 @@ import re
 line = 'asdf fjdk; afed, fjek,asdf, foo'
 new_line = re.split(r'[;,\s]\s*', line)
 print(new_line)
+
+fields = re.split(r'(;|,|\s)\s*', line)
+print(fields)
+fields = re.split(r'(?:;|,|\s)\s*', line)
+print(fields)
