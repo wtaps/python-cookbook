937f982bdbc5e037ac392f25f668fb18d60ae31e
wtaps <tonyleung187@gmail.com>
2015-08-04 14:27:06 +0800

    add 2.20.py

---
+++ 2/2.20.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+字节字符串上的字符串操作
+"""
+
+>>> data = b'Hello World'
+>>> data[0:5]
+b'Hello'
+>>> data.startswith(b'Hello')
+True
+>>> data.split()
+[b'Hello', b'World']
+>>> data.replace(b'Hello', b'Hello Cruel')
+b'Hello Cruel World'
+>>>
+
+
+>>> data = bytearray(b'Hello World')
+>>> data[0:5]
+bytearray(b'Hello')
+>>> data.startswith(b'Hello')
+True
+>>> data.split()
+[bytearray(b'Hello'), bytearray(b'World')]
+>>> data.replace(b'Hello', b'Hello Cruel')
+bytearray(b'Hello Cruel World')
+>>>
