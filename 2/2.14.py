5fd7be8a4041f2fa3a7510280b665d776b34ea72
wtaps <tonyleung187@gmail.com>
2015-08-04 11:49:24 +0800

    add 2.14.py

---
+++ 2/2.14.py
@@ -0,0 +1,69 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+合并拼接字符串
+"""
+
+
+>>> parts = ['Is', 'Chicago', 'Not', 'Chicago?']
+>>> ' '.joins(parts)
+'Is Chicago Not Chicago?'
+>>> ','.join(parts)
+'Is,Chicago,Not,Chicago?'
+>>> ''.join(parts)
+'IsChicagoNotChicago?'
+>>>
+
+
+>>> a = 'Is Chicago'
+>>> b = 'Not Chicago?'
+>>> a + ' ' + b
+'Is Chicago Not Chicago?'
+>>>
+
+
+>>> print('{} {}'.format(a,b))
+Is Chicago Not Chicago?
+>>> print(a + ' ' + b)
+Is Chicago Not Chicago?
+>>>
+
+
+>>> a = 'Hello' 'World'
+>>> a
+'HelloWorld'
+>>>
+
+
+>>> data = ['ACME', 50, 91.1]
+>>> ','.join(str(d) for d in data)
+'ACME,50,91.1'
+>>>
+
+print(a + ':' + b + ':' + c) # Ugly
+print(':'.join([a, b, c])) # Still ugly
+print(a, b, c, sep=':') # Better
+
+
+def sample():
+    yield 'Is'
+    yield 'Chicago'
+    yield 'Not'
+    yield 'Chicago?'
+
+def combine(source, maxsize):
+    parts = []
+    size = 0
+    for part in source:
+        parts.append(part)
+        size += len(part)
+        if size > maxsize:
+            yield ''.join(parts)
+            parts = []
+            size = 0
+        yield ''.join(parts)
+
+# 结合文件操作
+with open('filename', 'w') as f:
+    for part in combine(sample(), 32768):
+        f.write(part)
