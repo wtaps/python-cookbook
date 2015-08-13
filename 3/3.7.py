529b2ff3b3eafc2db672ed3d8a5c9e9cb3691090
wtaps <tonyleung187@gmail.com>
2015-08-13 11:13:30 +0800

    add 3.7.py

---
+++ 3/3.7.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+无穷大与NaN
+"""
+
+>>> a = float('inf')
+>>> b = float('-inf')
+>>> c = float('nan')
+>>> a
+inf
+>>> b
+-inf
+>>> c
+nan
+>>>
+
+
+>>> math.isinf(a)
+True
+>>> math.isnan(c)
+True
+>>>
