662e2041687f1b3313b93afbce287871cd43a25b
wtaps <tonyleung187@gmail.com>
2015-08-26 14:24:22 +0800

    add 4.1.py

---
+++ 4/4.1.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+手动遍历迭代器
+"""
+
+
+def manual_iter():
+    with open('/etc/hosts') as f:
+        try:
+            while True:
+                line = next(f)
+                print(line)
+        except StopIteration:
+            pass
+
+
+
+if __name__ == '__main__':
+    manual_iter()
