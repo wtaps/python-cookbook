8a27b548af1113ac87ab55014b7ed98421f77ea3
wtaps <tonyleung187@gmail.com>
2015-08-26 16:34:00 +0800

    add 4.4.py

---
+++ 4/4.4.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+实现迭代器协议
+"""
+
+
+class Node(object):
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+
+
+
+if __name__ == '__main__':
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+
+    for ch in root.depth_first():
+        print(ch)
