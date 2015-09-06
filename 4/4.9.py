d953d8b88851f532534a436d3b6299ac149fa03f
wtaps <tonyleung187@gmail.com>
2015-09-06 17:21:32 +0800

    add 4.9.py

---
+++ 4/4.9.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+排列组合的迭代
+"""
+
+
+>>> items = ['a', 'b', 'c']
+>>> from itertools import permutations
+>>> for p in permutations(items):
+...     print(p)
+...
+('a', 'b', 'c')
+('a', 'c', 'b')
+('b', 'a', 'c')
+('b', 'c', 'a')
+('c', 'a', 'b')
+('c', 'b', 'a')
+>>>
+
+
+>>> for p in permutations(items, 2):
+...     print(p)
+...
+('a', 'b')
+('a', 'c')
+('b', 'a')
+('b', 'c')
+('c', 'a')
+('c', 'b')
+>>>
