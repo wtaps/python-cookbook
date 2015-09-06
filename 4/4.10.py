4f65d0eedd373a43b9d40f28fcd951743015bb26
wtaps <tonyleung187@gmail.com>
2015-09-06 17:26:22 +0800

    add 4.10.py

---
+++ 4/4.10.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3.3
+# -*- coding: utf-8 -*-
+"""
+序列上索引值迭代
+"""
+
+
+
+>>> my_list = ['a', 'b', 'c']
+>>> for idx, val in enumerate(my_list):
+...     print(idx, val)
+...
+0 a
+1 b
+2 c
